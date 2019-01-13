e95f91403d02c6bd032cc5137e2e2bae0b591987
Njui <[snjui7@gmail.com]>
2019-01-13 14:38:20 +0300

    return to modify question records and create app in the module

--- app/tests/test_questionsrecords.py
+++ app/tests/test_questionsrecords.py
@@ -12,22 +12,22 @@ from app.api.v1.models.createmeetup import CreateMeetups
 class Test_Questions(unittest.TestCase):
     def setUp(self):
         
-        # # self.app = create_app()
-        # self.client = self.app.test_client()
-        # self.post_question = {
-        #     "question": "question",
-        #     "title": "title",
-        #     "meetup_id": "meetup_id",
-        #     "user_id": "user_id"
-        # }
+        # self.app = create_app()
+        self.client = self.app.test_client()
+        self.post_question = {
+            "question": "question",
+            "title": "title",
+            "meetup_id": "meetup_id",
+            "user_id": "user_id"
+        }
 
-        # def test_get_all_meetups(self):
-        #     result = self.client.get('/api/v1/questions', content_type='application/json')
-        # self.assertEqual(result.status_code, 200)
+        def test_get_all_meetups(self):
+            result = self.client.get('/api/v1/questions', content_type='application/json')
+        self.assertEqual(result.status_code, 200)
 
-    # def test_create_endpoint(self):
-    #     result = self.client.get('/api/v1/questions', content_type='application/json')
-    #     self.assertEqual(result.status_code, 201)
+    def test_create_endpoint(self):
+        result = self.client.get('/api/v1/questions', content_type='application/json')
+        self.assertEqual(result.status_code, 201)
 
-# if __name__ == '__main__':
-#     unittest.main()
+if __name__ == '__main__':
+    unittest.main()
