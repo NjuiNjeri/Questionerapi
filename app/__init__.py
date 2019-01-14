d1459a04d942bb08e46af0bbbd242c652683af0b
Njui <[snjui7@gmail.com]>
2019-01-14 22:48:35 +0300

    finally the question records test has passed! for both  endpoints

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from app.api.v1.views.meetviews import v1_meetups as v1
-
+from app.api.v1.views.questionviews import v1_all_questions_blueprint
 
 def  create_app():
     app = Flask(__name__)
     app.register_blueprint(v1)
+    app.register_blueprint(v1_all_questions_blueprint)
     return app
--- app/api/v1/models/questionrecords.py
+++ app/api/v1/models/questionrecords.py
@@ -4,20 +4,24 @@ all_questions = []
 
 class AllQuestions():
     
-    def __init__(self, *args,):
+    def __init__(self):
         self.records = all_questions
 
-        def post_question(self, meetupid, title, question):
-            ''' will add a new question to the self.all_questions list'''
+    def post_question(self, meetupid, title, question):
+        ''' will add a new question to the self.all_questions list'''
 
-            new_question= {
-                "questionid": len(self.all_questions)+1,
-                "date": datetime.now(),
-                "meetupid": meetupid,
-                "title": title,
-                "question": question               
+        new_question= {
+            "questionid": len(self.records)+1,
+            "date": datetime.now(),
+            "meetupid": meetupid,
+            "title": title,
+            "question": question               
 
-            }
+        }
 
-            self.records.append(new_question)
-            return self.records    
+        self.records.append(new_question)
+        return self.records    
+
+    def get_all_questions(self):
+    
+        return self.records
