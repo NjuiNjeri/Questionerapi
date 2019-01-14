a09afb08d73dbea2b3a16b9ce8521a62a858717c
Njui <[snjui7@gmail.com]>
2019-01-14 11:20:40 +0300

    httpresponse for question views

--- app/api/v1/views/questionviews.py
+++ app/api/v1/views/questionviews.py
@@ -2,6 +2,7 @@ from flask import jsonify, Blueprint, request, json
 from ..models.questionrecords import All_Questions
 from datetime import datetime
 from uuid import uuid4
+from django.http import HttpResponse, HttpResponseNotFound
 
 v1_all_questions_blueprint= Blueprint('questions', __name__, url_prefix='api/v1')
 
@@ -11,7 +12,7 @@ def post_question(id):
     data= request.get_json()
     title = data['title']
     question=data['question']
-    
 
-    return post_question.all_questions
+
+    return HttpResponse(status=201)
 
