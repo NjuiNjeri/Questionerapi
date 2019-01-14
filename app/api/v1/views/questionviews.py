2eeb77a386d83ddeda527fc9872d67f45d8f8018
Njui <[snjui7@gmail.com]>
2019-01-14 10:54:12 +0300

    progress on test question records

--- app/api/v1/views/questionviews.py
+++ app/api/v1/views/questionviews.py
@@ -4,8 +4,11 @@ from datetime import datetime
 from uuid import uuid4
 
 v1_all_questions_blueprint= Blueprint('questions', __name__, url_prefix='api/v1')
+newquestion=All_Questions()
 
 @v1_all_questions_blueprint.route('/questions', methods=['POST'])
 def post_question():
     data= request.get_json()
+
+    return post_question.all_questions
 
