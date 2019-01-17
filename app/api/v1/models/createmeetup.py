5e88701cf87bb046f6ddf145364be443c41c0455
Njui <[snjui7@gmail.com]>
2019-01-17 12:40:53 +0300

    Almost there with the meetups endpoint tests

--- app/api/v1/models/createmeetup.py
+++ app/api/v1/models/createmeetup.py
@@ -8,17 +8,26 @@ class CreateMeetups():
     def __init__(self):
         self.meetups = []
 
-    def createMeetup(self, meetup_title="", meetup_time= "", meetup_venue=""):
+    def createMeetup(self, meetupid="", meetup_title="", meetup_time= "", meetup_venue=""):
         '''creates new meetup'''
         newMeetup ={
+            'meetupid': meetupid,
             'meetupdate': datetime,
             'meetuptime': meetup_time,
             'meetupvenue': meetup_venue,
          }
 
         self.meetups.append(newMeetup)
+        return jsonify(self.meetups)
 
     def post_meetups(self):
         return self.meetups
+
+    def delete_meetup(self, meetupId):
+        """ Deletes a specific meetup record """
+        Meetup = [Meetup for Meetup in self.meetups if Meetup['meetupId'] == meetupId]
+        if Meetup:
+            self.meetups.remove(Meetup[0])
+        return abort(400, "Error: meetup {} doesn't exist!".format(meetupId))
     
        
--- app/api/v1/views/meetviews.py
+++ app/api/v1/views/meetviews.py
@@ -3,24 +3,26 @@ from datetime import datetime
 from uuid import uuid4
 from ..models.createmeetup import CreateMeetups
 
-v1_mymeetups_blueprint = Blueprint('/mymeetups', __name__, url_prefix='/api/v1')
+v1_mymeetups_blueprint = Blueprint('meetups', __name__, url_prefix='/api/v1')
 
 meetup = CreateMeetups()
 
-@v1_mymeetups_blueprint.route('/mymeetups', methods=['POST'])
-def my_meetups():
+@v1_mymeetups_blueprint.route('/meetups', methods=['POST'])
+def post_meetups():
     data= request.get_json()
-    title = data['meetup_title']
-    venue = data['meetup_venue']
+    title = data['title']
+    venue = data['venue']
     time = data['time']
-    response = meetup.post_meetups(title, venue, time)
-    print(response)
-
-    return make_response(jsonify({"Meetup record updated" : response})), 201
+   
+    meetups= meetup.createMeetup(title, venue, time)
+    return jsonify({"status": 201, "data": meetups}),201
     
-@v1_mymeetups_blueprint.route('/mymeetups', methods=['GET'])
+@v1_mymeetups_blueprint.route('/meetups', methods=['GET'])
 def get_all():
     views =  meetup.post_meetups()
     return jsonify(views)
 
-
+@v1_mymeetups_blueprint.route('/meetups/delete/<int:meetupId>', methods=['DELETE'])
+def delete_meetup(meetupId):
+    Meetup = meetup.delete_meetup(meetupId)
+    return jsonify({"status": 204, "Meetup": Meetup})
